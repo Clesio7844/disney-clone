d1a667660be1b6e13f0d5978057cf4ddf0d033e6
david <daviddecastro42@live.co.uk>
2021-04-27 15:39:37 +0100

    finished on login component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
+import Login from './components/Login';
 
 function App() {
   return (
     <div className='App'>
-      <h1>hello world</h1>
+      <Router>
+        <Switch>
+          <Route path='/'>
+            <Login />
+          </Route>
+          {/* <Route path='/users'>
+              <Users />
+            </Route>
+            <Route path='/'>
+              <Home />
+            </Route> */}
+        </Switch>
+      </Router>
     </div>
   );
 }
